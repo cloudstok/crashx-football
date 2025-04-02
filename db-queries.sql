d76573d719ec1a2edcf634a66ea6de5b4ef86c5b
Prashantg30 <prashant.gupta@cloudstok.com>
2025-04-02 11:26:01 +0000

    prod changes committed

--- db-queries.sql
+++ db-queries.sql
@@ -11,9 +11,11 @@ use `crashx_football_game`;
    `operator_id` varchar(255) DEFAULT NULL,
    `name` varchar(255) DEFAULT NULL,
    `bet_amount` DECIMAL(10, 2) DEFAULT 0.00,
+   `auto_cashout` DECIMAL(10, 2) NULL DEFAULT NULL,
    `avatar` VARCHAR(255) NOT NULL,
    `balance` DECIMAL(10, 2) DEFAULT 0.00,
    `max_mult` DECIMAL(10, 2) DEFAULT 0.00,
+    `win_amount` DECIMAL(10, 2) DEFAULT 0.00,
    `status` varchar(255) DEFAULT "CRASHED",
    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
    PRIMARY KEY (`settlement_id`)
@@ -56,6 +58,7 @@ CREATE TABLE IF NOT EXISTS `bets` (
    `operator_id` varchar(255) DEFAULT NULL,
    `name` VARCHAR(45) NOT NULL,
    `bet_amount` DECIMAL(10, 2) DEFAULT 0.00,
+   `auto_cashout` DECIMAL(10, 2) NULL DEFAULT NULL,
    `avatar` VARCHAR(255) NULL ,
    `balance` DECIMAL(10, 2) DEFAULT 0.00,
    `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -65,7 +68,3 @@ CREATE TABLE IF NOT EXISTS `bets` (
 ALTER TABLE `bets` ADD INDEX `lobby_id_index` (`lobby_id` ASC)VISIBLE, ADD INDEX `operator_id_index` (`operator_id` ASC) VISIBLE, ADD INDEX `bet_amount_index` (`bet_amount` ASC) VISIBLE, ADD INDEX `created_at_index` (`created_at` ASC) VISIBLE;
 ALTER TABLE `round_stats` ADD INDEX `lobby_id_index` (`lobby_id` ASC) VISIBLE, ADD INDEX `max_mult_index` (`max_mult` ASC) VISIBLE, ADD INDEX `created_at_index` (`created_at` ASC) VISIBLE;
 ALTER TABLE `settlement` ADD INDEX `lobby_id_index` (`lobby_id` ASC) INVISIBLE,ADD INDEX `bet_amount_index` (`bet_amount` ASC) INVISIBLE, ADD INDEX `max_mult_index` (`max_mult` ASC) VISIBLE;
-
-
-ALTER TABLE `bets` ADD COLUMN `auto_cashout` DECIMAL(10, 2) NULL DEFAULT NULL AFTER `bet_amount`;
-ALTER TABLE `settlement` ADD COLUMN `auto_cashout` DECIMAL(10, 2) NULL DEFAULT NULL AFTER `bet_amount`;
